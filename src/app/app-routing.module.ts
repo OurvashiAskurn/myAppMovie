1fb2af1ed0f0573c961a4a07347198ae23e490f1
Askurn <ourvashi.askurn@accenture.com>
2020-10-14 16:10:03 +0400

    c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginComponent } from './components/login/login.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent, pathMatch: 'full'},
   {path: '', redirectTo:'new_release', pathMatch: 'full'},
   {path: 'new_release', children: [
-    {path: '', component: MovieListComponent},
+    {path: '', component: HomePageComponent},
     {path: ':id', children: [
       {path: '', component: MovieDetailComponent}
     ]}
   ]},
   {path: 'trending', children: [
-    {path: '', component: MovieListComponent},
+    {path: '', component: HomePageComponent},
     {path: ':id', children: [
       {path: '', component: MovieDetailComponent}
     ]}
   ]},
   {path: 'coming_soon', children: [
-    {path: '', component: MovieListComponent},
+    {path: '', component: HomePageComponent},
     {path: ':id', children: [
       {path: '', component: MovieDetailComponent}
     ]}
   ]},
   {path: 'favourites', children: [
-    {path: '', component: MovieListComponent},
+    {path: '', component: HomePageComponent},
     {path: ':id', children: [
       {path: '', component: MovieDetailComponent}
     ]}
   ]},
   {path: 'watch_later', children: [
-    {path: '', component: MovieListComponent},
+    {path: '', component: HomePageComponent},
     {path: ':id', children: [
       {path: '', component: MovieDetailComponent}
     ]}
