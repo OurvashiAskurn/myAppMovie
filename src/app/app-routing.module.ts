c97358f373106a6ed8459dcc48b717407a453741
Askurn <ourvashi.askurn@accenture.com>
2020-10-14 10:07:11 +0400

    c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './components/home-page/home-page.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'new_release', pathMatch: 'full'},
   {path: 'new_release', children: [
-    {path: '', component: HomePageComponent},
+    {path: '', component: MovieListComponent},
     {path: 'id', component: MovieDetailComponent}
   ]},
   {path: 'trending', children: [
